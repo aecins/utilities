21d0299818c04dac1461572c53ded9d090cbd6d2
Aleksandrs Ecins <aleksandrs.ecins@gmail.com>
2014-04-25 14:52:20 -0400

    added description

--- my_code/clearAllPreserveBreakpoints.m
+++ my_code/clearAllPreserveBreakpoints.m
@@ -1,5 +1,12 @@
 function clearAllPreserveBreakpoints()
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Clear all variables without deleting the existing breakpoints.
+%
+% ----------------
+% Aleksandrs Ecins
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 current_breakpoints = dbstatus('-completenames');
 evalin('base', 'clear all');
 dbstop(current_breakpoints );
+
 end
